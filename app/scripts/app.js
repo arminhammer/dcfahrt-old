040e067feeb334d3f881126128247991138f4134
arminhammer <arminhammer@gmail.com>
2014-12-11 21:37:40 -0500

    Changing to a tabbed design.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,12 @@
 'use strict';
-// Ionic Starter App, v0.9.20
+// Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('Dcfahrt', ['ionic', 'config', 'Dcfahrt.controllers'])
+angular.module('Dcfahrt', ['ionic', 'config', 'Dcfahrt.controllers', 'Dcfahrt.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,52 +23,73 @@ angular.module('Dcfahrt', ['ionic', 'config', 'Dcfahrt.controllers'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    .state('app', {
-      url: '/app',
+    // setup an abstract state for the tabs directive
+    .state('tab', {
+      url: '/tab',
       abstract: true,
-      templateUrl: 'templates/menu.html',
-      controller: 'AppCtrl'
+      templateUrl: 'templates/tabs.html'
     })
 
-    .state('app.search', {
-      url: '/search',
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+      url: '/dash',
       views: {
-        'menuContent' :{
-          templateUrl: 'templates/search.html'
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
         }
       }
     })
 
-    .state('app.browse', {
-      url: '/browse',
+    .state('tab.home', {
+      url: '/home',
       views: {
-        'menuContent' :{
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+        'tab-dash': {
+          templateUrl: 'templates/tab-home.html',
+          controller: 'HomeCtrl'
         }
       }
     })
 
-    .state('app.single', {
-      url: '/playlists/:playlistId',
+    .state('tab.friends', {
+      url: '/friends',
       views: {
-        'menuContent' :{
-          templateUrl: 'templates/playlist.html',
-          controller: 'PlaylistCtrl'
+        'tab-friends': {
+          templateUrl: 'templates/tab-friends.html',
+          controller: 'FriendsCtrl'
+        }
+      }
+    })
+    .state('tab.friend-detail', {
+      url: '/friend/:friendId',
+      views: {
+        'tab-friends': {
+          templateUrl: 'templates/friend-detail.html',
+          controller: 'FriendDetailCtrl'
+        }
+      }
+    })
+
+    .state('tab.account', {
+      url: '/account',
+      views: {
+        'tab-account': {
+          templateUrl: 'templates/tab-account.html',
+          controller: 'AccountCtrl'
         }
       }
     });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/tab/home');
+
 });
 
